fc7029aa8e6eae26e6d4ccdfc358e0390c62a784
Suraj <29368267+geekboysuraj@users.noreply.github.com>
2025-08-04 11:57:38 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,22 +3,23 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "25712887"))
-API_HASH = environ.get("API_HASH", "e63bb82b544af7426b29d62939d99291")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7860223453:AAHvXa1pmrMI623LB0Eo2u6R1gThkQpLxHg")
+API_ID = int(environ.get("API_ID", "25712888"))
+API_HASH = environ.get("API_HASH", "e63bb82b544af7426b29d62939d99292")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7860223453:AAHvXa1pmrMI623LB0Eo2u6R1gThkQpLxHh")
 
-OWNER = int(environ.get("OWNER", "1919432812"))
+OWNER = int(environ.get("OWNER", "1919432813"))
 CREDIT = environ.get("CREDIT", "SURAJ YADAV")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '1919432812').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '1919432813').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '1919432812').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '1919432813').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
